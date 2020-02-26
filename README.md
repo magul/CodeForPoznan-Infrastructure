967f306796275f844887290c6510f873e686604d
Tomasz Magulski <tomasz@magul.ski>
2020-02-26 10:02:41 +0100

    Main postgres DB (#34)

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Terraform version:
 ```
 Terraform v0.12.20
 + provider.aws v2.36.0
++ provider.random v2.2.1
++ provider.template v2.1.2
 
 ```
 Initialize project
---
+++ db.tf
@@ -0,0 +1,38 @@
+resource "aws_db_subnet_group" "private" {
+  name       = "private"
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+    aws_subnet.private_c.id,
+  ]
+}
+
+resource "random_password" "db_password" {
+  length  = 128
+  special = false
+}
+
+resource "aws_db_instance" "db" {
+  identifier              = "main-postgres"
+
+  engine                  = "postgres"
+  engine_version          = "9.5"
+
+  instance_class          = "db.t2.micro"
+  allocated_storage       = 8
+
+  username                = "postgres"
+  password                = random_password.db_password.result
+
+  backup_retention_period = 7
+  backup_window           = "03:00-04:00"
+  multi_az                = false
+  db_subnet_group_name    = aws_db_subnet_group.private.name
+
+  final_snapshot_identifier = "main-postgres-final-snapshot"
+
+  depends_on = [
+    random_password.db_password,
+    aws_db_subnet_group.private,
+  ]
+}
