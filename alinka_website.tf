cd4dc15df49bb47a1528f10b7f2e7b18f50f70f3
Tomasz Magulski <tomasz@magul.ski>
2020-03-05 16:48:33 +0100

    Terraform alinka.io domain (#47)
    ... and production alinka website

--- alinka_website.tf
+++ alinka_website.tf
@@ -10,10 +10,33 @@ resource "aws_route53_zone" "alinka_website" {
   name = "alinka.io"
 }
 
+resource "aws_route53_record" "ns_alinka_website" {
+  zone_id = aws_route53_zone.alinka_website.zone_id
+  name    = aws_route53_zone.alinka_website.name
+  type    = "NS"
+  ttl     = "172800"
+  records = [
+    "${aws_route53_zone.alinka_website.name_servers.0}.",
+    "${aws_route53_zone.alinka_website.name_servers.1}.",
+    "${aws_route53_zone.alinka_website.name_servers.2}.",
+    "${aws_route53_zone.alinka_website.name_servers.3}.",
+  ]
+}
+
+resource "aws_route53_record" "soa_alinka_website" {
+  zone_id = aws_route53_zone.alinka_website.zone_id
+  name    = aws_route53_zone.alinka_website.name
+  type    = "SOA"
+  ttl     = "900"
+  records = [
+    "ns-1143.awsdns-14.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400",
+  ]
+}
+
 module alinka_website_ssl_certificate {
   source = "./ssl_certificate"
 
-  domain       = "dev.alinka.io"
+  domain       = "alinka.io"
   route53_zone = aws_route53_zone.alinka_website
 }
 
@@ -29,7 +52,7 @@ module alinka_website_cloudfront_distribution {
   source = "./cloudfront_distribution"
 
   name            = "alinka_website"
-  domain          = "dev.alinka.io"
+  domain          = "alinka.io"
   s3_bucket       = aws_s3_bucket.codeforpoznan_public
   route53_zone    = aws_route53_zone.alinka_website
   iam_user        = aws_iam_user.alinka_website
---
+++ dev_alinka_website.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_user" "dev_alinka_website" {
+  name = "dev_alinka_website"
+}
+
+resource "aws_iam_access_key" "dev_alinka_website" {
+  user = aws_iam_user.dev_alinka_website.name
+}
+
+module dev_alinka_website_ssl_certificate {
+  source = "./ssl_certificate"
+
+  domain       = "dev.alinka.io"
+  route53_zone = aws_route53_zone.alinka_website
+}
+
+module dev_alinka_website_frontend_assets {
+  source = "./frontend_assets"
+
+  name      = "dev_alinka_website"
+  s3_bucket = aws_s3_bucket.codeforpoznan_public
+  iam_user  = aws_iam_user.dev_alinka_website
+}
+
+module dev_alinka_website_cloudfront_distribution {
+  source = "./cloudfront_distribution"
+
+  name            = "dev_alinka_website"
+  domain          = "dev.alinka.io"
+  s3_bucket       = aws_s3_bucket.codeforpoznan_public
+  route53_zone    = aws_route53_zone.alinka_website
+  iam_user        = aws_iam_user.dev_alinka_website
+  acm_certificate = module.dev_alinka_website_ssl_certificate.certificate
+
+  origins = {
+    static_assets = {
+      default     = true
+      domain_name = aws_s3_bucket.codeforpoznan_public.bucket_domain_name
+      origin_path = "/dev_alinka_website"
+    }
+  }
+}
