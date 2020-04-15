42b73d6cf4e7dea0551f7bcaddebdb2bf6fedd05
Artur Tamborski <tamborskiartur@gmail.com>
2020-04-15 20:04:09 +0200

    Path+ -> Proxy+

--- serverless_api/main.tf
+++ serverless_api/main.tf
@@ -64,7 +64,7 @@ resource "aws_api_gateway_integration" "root_integration" {
 resource "aws_api_gateway_resource" "proxy_resource" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "{path+}"
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy_method" {
