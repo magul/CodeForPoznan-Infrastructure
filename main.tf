d328e3f3282663abb281b05894c8bf4b4bf57b7e
Artur Tamborski <tamborskiartur@gmail.com>
2020-07-07 21:55:37 +0200

    Add missing change from master branch

--- main.tf
+++ main.tf
@@ -44,32 +44,6 @@ data "aws_iam_policy_document" "codeforpoznan_public_policy" {
   }
 }
 
-data "aws_iam_policy_document" "codeforpoznan_public_policy" {
-  version = "2012-10-17"
-
-  statement {
-    sid    = "PublicListBucket"
-    effect = "Allow"
-    principals {
-      identifiers = ["*"]
-      type        = "*"
-    }
-    actions   = ["s3:ListBucket"]
-    resources = ["arn:aws:s3:::codeforpoznan-public"]
-  }
-
-  statement {
-    sid    = "PublicGetObject"
-    effect = "Allow"
-    principals {
-      identifiers = ["*"]
-      type        = "*"
-    }
-    actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::codeforpoznan-public/*"]
-  }
-}
-
 // shared public bucket (we will push here all static assets in separate directories)
 resource "aws_s3_bucket" "codeforpoznan_public" {
   bucket = "codeforpoznan-public"
