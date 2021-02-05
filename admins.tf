c0049d93a4845a1a308618e2e5a7252d06443020
Tomasz Magulski <tomasz@magul.ski>
2021-02-05 13:46:13 +0100

    Wistler as a devops engineer

--- admins.tf
+++ admins.tf
@@ -1,13 +1,20 @@
 module "tomasz_magulski" {
-  source = "./admin"
+  source     = "./admin"
 
   name       = "tomasz_magulski"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDWPsttMZ034NyxrnUahSlWzIlgJzAnPl+xn9dF1zocOroVRQQcMsn1jg9YD/HecSJeWHSh6J+m9qY4h4o1KNJ6AXGYAL5t526G9IrdDS0H5infSdT+YJ//K6Q2cHRbgrVV3eaWrP/vKJiN7lMFhNMow4GSqt9V8bMWzHFg9Jf6GN9AEkmLMuewxi/SFct8ZvWk8RgiJ1iWY3osOWBEsqZiAixolaqFIvK6SjxF6RlsvZXDGVyx1UPu3mSneph50i1xRDMu2J7O4NqdcRy3O6yZ65mzHPeL9wkxCWIYNbZ4YCwtShIzdKK83jSnqbB05U85yNrAi59hubVNCPGBntH9/2bADog2A74XdMoDjmPpztP8YCZxusuGGvWX5iA5G7OERfoS8vTYYasFxp2U8bkWukwU0Yg3h8Jt34Ri9GjKQ9vGCkqhrFaF+goSNIVYD8eU6hhvjlIZYzn6ismaq3ntGCijyZ/TwdBazjFa+sps9JtbUzy4MDl35Gv8wpp23qV/kd8Pw2qI7hJgjK/fxU3uj2RnEdLEPB1JeHi07Jhp3P6Dg5nMfdYRz6Y72xvhPPy1u/pxi2LFRdZklgz5IKc2a76ObqBETjqGULYAKYVh7P5+M77o/1BX2yO7VseRJgjHAXnZrCWSHdWhNFKQeo9rYX+ixRvawau5UujT4s5pOw== magul@bohr"
 }
 
 module "artur_tamborski" {
-  source = "./admin"
+  source     = "./admin"
 
   name       = "artur_tamborski"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxty3Zb4fxJKVyKI7gXLw+PkI/BAjrILZyo25IrzUv6aq8x0mJs49k+WqnWhFbzHLuGeNi2cddpZnSvf2JozczbZnjBjBIIwEqowLuPhCjBWdeB4UBI0KFigCMvPssecfnXVwwSD0kb1w+9dvqC7FHwLZ2glBM27emZRaraSiI78AiytdZT2UBhs6kDwJpr8XswZG0uSbNVANN3fY8fIffmVSpsggw8Fw0WdlZGm3qFzj0FRPsrFbA6RBitAvdhbeITJU+fw6sA8FKKpXZOMlg7nUCt4jiPc7VMaAULamMTISPofkvFI6kG8jgewJyrwz9h9vEbsvOKMU9fxvDtK2ffabKru/7onEn6NXwm2Wg+OejdS5YzllGq4O3dcGDG1fNKOW9JZQhYIbKRL62/YAMQ+8Jz/xHq2992VDy20uegZavjLJfFlINTe9g3hFHxnEuoiH6ZhqHCGUokCwvWiyKaIlpotvPY+JQnKk3NgNCyV8BO4u02rmRrJUX4/Xxh35+TVirY3VBUCMTB+1T+6QwgpYM7S9bH+TkLRCQIQFk64A5aWkao+zWlDSTAv7eYsQ7ISdwQyqOmLvqI6H1pwEnCam8K/0cn4jX1j6uGllaxtU9KhQXUGPoQtKZ8xQqHSxeNg6R/rBwudIbeGwAilYsXDBvwLFjLHgmHA2cBmnk6Q== tamborskiartur@gmail.com"
+}
+
+module "wojciech_patelka" {
+  source     = "./admin"
+
+  name       = "wojciech_patelka"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDtxQf6YmBwhR2Hawo2Z9KNSGUnHTg6uerdfOj5JRR2ThXt7qCcvyuCkEO1kuYheuNXcoclmy6Z9iQWHjcNGj0cMsD8fX/fyxpa5LNX39pxtEadTutqaradP6yHIgdHMgoAN9jsPQ3j/J3C6g/ReV/X7A3uHWYI2Xoq2FRKwOplV6hdbrSEvpZYPPA0zhmtqNi71GyPUqLkYJmUDXZmpT0RSZat0WFdwuSJfAt9A//LnLd+IhRP6gUJPOrksGWf2TH4LO40yMse5psX7ZchdaNEbCuJ0EkUTAHlfdpSPy3p8qxfpAC9TxUFSWIrf2VNpi86kjy8nqC35qb7Oh7kcVMS1/G7bfSvRCOaDRbtjiM2zQjEmhGr+rfvRikowMow7pjMk/Zo69ZpOGenDcekd3Tp9U6H6T0zM6bVuxv89TjLe9WoCkfvHPJWwcdtQ0uaAQwZPgRACcalMGanATbCFfCt0PGN7gLg9a5D6qQ+p7s8OMPLVzDSbg/9m9Hs22Mc0+s= wistler@mbp-wistler.local"
 }
--- ssl_certificate/main.tf
+++ ssl_certificate/main.tf
@@ -15,11 +15,19 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 resource "aws_route53_record" "certificate_validation_record" {
-  name    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type
-  zone_id = var.route53_zone.id
-  records = [aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value]
-  ttl     = 60
+  for_each   = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name       = each.value.name
+  type       = each.value.type
+  zone_id    = var.route53_zone.id
+  records    = [each.value.record]
+  ttl        = 60
 
   depends_on = [
     aws_acm_certificate.certificate,
@@ -29,7 +37,9 @@ resource "aws_route53_record" "certificate_validation_record" {
 
 resource "aws_acm_certificate_validation" "certificate_validation" {
   certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [aws_route53_record.certificate_validation_record.fqdn]
+  validation_record_fqdns = [
+    for record in aws_route53_record.certificate_validation_record : record.fqdn
+  ]
   provider                = aws.north_virginia
 
   depends_on = [
