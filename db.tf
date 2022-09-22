eb01f6e76cd28d61c17ac499082cc8e1945362cd
Artur Tamborski <tamborskiartur@gmail.com>
2022-09-22 15:59:19 +0200

    aws bumped the postgres

--- db.tf
+++ db.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "db" {
   identifier = "main-postgres"
 
   engine         = "postgres"
-  engine_version = "12.8"
+  engine_version = "12.11"
 
   instance_class    = "db.t2.micro"
   allocated_storage = 8
